49c2ed6c3ded5b39fa6369606a9d2a601215ff49
stonarini <stonarini@cifpfbmoll.eu>
2022-02-17 18:48:38 +0100

    feat(ItemFactory): add static class ItemFactory to manage Items

---
+++ src/main/java/edu/poniperro/galleygrub/items/ItemFactory.java
@@ -0,0 +1,31 @@
+package edu.poniperro.galleygrub.items;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ItemFactory {
+    private static final Map<String, Item> itemMap = new HashMap<String, Item>();
+
+    public static Item getItem(String name, Double price) {
+        if (!itemMap.containsKey(name)) {
+            itemMap.put(name, new Item(name, price));
+        }
+        return itemMap.get(name);
+    }
+
+    public static Item getItem(String name, Double price, String extra) {
+        String key = name + " " + extra;
+        if (!itemMap.containsKey(key)) {
+            itemMap.put(key, new Item(name, price));
+        }
+        return itemMap.get(key);
+    }
+
+    public static int size() {
+        return itemMap.size();
+    }
+
+    public static void clear() {
+        itemMap.clear();
+    }
+}
