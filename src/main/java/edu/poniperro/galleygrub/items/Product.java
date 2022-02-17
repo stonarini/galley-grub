4113c4e079bb9fbae7e3b96cb81b5ee28371213b
stonarini <stonarini@cifpfbmoll.eu>
2022-02-17 17:41:54 +0100

    feat(Product): add Product interface for Item class

---
+++ src/main/java/edu/poniperro/galleygrub/items/Product.java
@@ -0,0 +1,11 @@
+package edu.poniperro.galleygrub.items;
+
+public interface Product {
+    public String name();
+
+    public Double price();
+
+    public String extra();
+
+    public Boolean isRegular();
+}
