810bb29e392e14863587e1333491f4022a75c926
stonarini <stonarini@cifpfbmoll.eu>
2022-02-17 18:25:22 +0100

    feat(Comanda): add itemList method to Comanda interface

--- src/main/java/edu/poniperro/galleygrub/order/Comanda.java
+++ src/main/java/edu/poniperro/galleygrub/order/Comanda.java
@@ -1,9 +1,15 @@
 package edu.poniperro.galleygrub.order;
 
+import java.util.List;
+
+import edu.poniperro.galleygrub.items.Item;
+
 public interface Comanda {
     void addItem(String name, Double price);
 
     void addItem(String name, Double price, String extra);
+
+    List<Item> itemList();
 
     void display();
 }
