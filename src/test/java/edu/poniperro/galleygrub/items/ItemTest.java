9eb76f0f9e985d6d07e73653150360311458d8d2
stonarini <stonarini@cifpfbmoll.eu>
2022-02-17 17:37:04 +0100

    test(Item): add tests for Item class

---
+++ src/test/java/edu/poniperro/galleygrub/items/ItemTest.java
@@ -0,0 +1,29 @@
+package edu.poniperro.galleygrub.items;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemTest {
+    Item krabbyPatty;
+
+    @Before
+    public void setup() {
+        this.krabbyPatty = new Item("Krabby Patty", 1.25);
+    }
+
+    @Test
+    public void isRegularItemTest() {
+        assertTrue(krabbyPatty.isRegular());
+    }
+
+    @Test
+    public void itemEqualsItemTest() {
+        Item krabbyMeal = new Item("Krabby Meal", 3.5);
+        Item otherKrabbyPatty = new Item("Krabby Patty", 1.25);
+        assertFalse(krabbyPatty.equals(krabbyMeal));
+    }
+
+}
