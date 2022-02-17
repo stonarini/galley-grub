9cac3b4459dbd6246e0dce6bd551855d8193c1b4
stonarini <stonarini@cifpfbmoll.eu>
2022-02-17 21:04:36 +0100

    feat(Ticket, Receipt): add last methods

--- src/main/java/edu/poniperro/galleygrub/receipt/Receipt.java
+++ src/main/java/edu/poniperro/galleygrub/receipt/Receipt.java
@@ -1,10 +1,11 @@
 package edu.poniperro.galleygrub.receipt;
 
+import edu.poniperro.galleygrub.extras.Extra;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public class Receipt implements Ticket {
-    private Double total;
     private Comanda comanda;
+    private Extra firstExtra;
 
     public Receipt(Comanda comanda) {
         this.comanda = comanda;
@@ -16,8 +17,23 @@ public class Receipt implements Ticket {
     }
 
     @Override
+    public void setChain(Extra firstExtra) {
+        this.firstExtra = firstExtra;
+    }
+
+    @Override
+    public Extra getChain() {
+        return this.firstExtra;
+    }
+
+    @Override
     public Double total() {
         return comanda.getTotal();
+    }
+
+    @Override
+    public void sumExtrasCharge() {
+        firstExtra.sumExtras(comanda);
     }
 
     @Override
--- src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
+++ src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
@@ -1,9 +1,16 @@
 package edu.poniperro.galleygrub.receipt;
 
+import edu.poniperro.galleygrub.extras.Extra;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public interface Ticket {
     public Comanda getOrder();
+
+    public void setChain(Extra firsExtra);
+
+    public Extra getChain();
+
+    public void sumExtrasCharge();
 
     public Double total();
 
